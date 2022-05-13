85f0235aab0796ec61333fde06d488c28c11af9c
darvesh2312 <darvesh@whozzat.com>
2022-05-13 19:07:37 +0530

    req.txt

--- weather/models.py
+++ weather/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class City(models.Model):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
     name = models.CharField(max_length=25)
 
     def __str__(self):
